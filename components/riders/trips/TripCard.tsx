7148f06a52c06f59ca41bdc41617947edd8297b6
Noah Silvera <noah@super.gd>
2021-03-25 18:58:49 -0700

    Add trip dates to TripCard

--- components/riders/trips/TripCard.tsx
+++ components/riders/trips/TripCard.tsx
@@ -5,8 +5,9 @@ export default function TripCard({ trip, className = "", onClick = null }) {
 
   return (
     <div className={className} onClick={onClick} >
-      <div className="flex items-center my-2 mx-8 py-2 bg-white hover:bg-gray-200 rounded-3xl flex-row justify-center">
-        {trip.name}
+      <div className="flex items-center my-2 mx-8 py-2 bg-white hover:bg-gray-200 rounded-3xl flex-col justify-center">
+        <p>{trip.name}</p>
+        <p>{new Date(trip.start_date).toDateString()} - {trip.end_date ? new Date(trip.end_date).toDateString() : "ongoing"}</p>
        </div>
     </div>
   )
