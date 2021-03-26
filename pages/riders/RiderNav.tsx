99c5b3722065118c243042491eb1c572823b5982
Noah Silvera <noah@super.gd>
2021-03-25 17:47:18 -0700

    Display rider name in Nav
    This uses the new NavArrow and NavSeparator components to nicely display
    the riders  name in the navbar.

--- pages/riders/RiderNav.tsx
+++ pages/riders/RiderNav.tsx
@@ -1,14 +1,19 @@
 import { useRouter } from 'next/router'
-import Nav from '../../components/Nav'
+import NavArrow from '../../components/NavArrow'
 import NavLink from '../../components/NavLink'
+import NavSeparator from '../../components/NavSeparator'
+import RootNav from '../../components/RootNav'
 
 export default function RiderNav({ rider }){
   const router = useRouter()
 
   return (
-    <Nav>
+    <RootNav>
+      <NavArrow/>
+      <NavLink onClick={() => router.push(`/riders/${rider.user_id}`)} is_active_path={`/riders/${rider.user_id}`} className="italic">{rider.first_name} {rider.last_name}</NavLink>
+      <NavSeparator/>
       <NavLink onClick={() => router.push(`/riders/${rider.user_id}/trips`)} is_active_path={`/riders/${rider.user_id}/trips`} >Trips</NavLink>
       <NavLink onClick={() => router.push(`/riders/${rider.user_id}/stats`)} is_active_path={`/riders/${rider.user_id}/stats`}>Stats</NavLink>
-    </Nav>
+    </RootNav>
   )
 }
