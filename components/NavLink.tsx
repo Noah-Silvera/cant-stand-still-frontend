8b6f1b251a4d93c7bc5804bcbd430965194b4e5a
Noah Silvera <noah@super.gd>
2021-03-25 17:46:26 -0700

    Allow NavLink to accept external classes

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function NavLink({ onClick = null, href = null, children = null, is_active_path = null }) {
+export default function NavLink({ onClick = null, href = null, children = null, is_active_path = null, className = "" }) {
   const [is_active, setIsActive] = useState(false);
 
   useEffect(() => {
@@ -13,8 +13,13 @@ export default function NavLink({ onClick = null, href = null, children = null, 
     <a
       href={href}
       onClick={onClick}
-      className={`block mt-4 lg:inline-block lg:mt-0 mr-4 ${is_active ? "text-white": "text-teal-200 hover:text-white"}`}>
-        {children}
+      className={`
+        block mt-4 lg:inline-block lg:mt-0 mr-4
+         ${is_active ? "text-white": "text-teal-200 hover:text-white"}
+         ${className}
+      `}
+    >
+      {children}
     </a>
   )
 }
