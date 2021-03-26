53a1806c9d7230c0dfb2dc9bd6b118c3d6fa939f
Noah Silvera <noah@super.gd>
2021-03-25 18:59:47 -0700

    Remove stats link

--- pages/riders/RiderNav.tsx
+++ pages/riders/RiderNav.tsx
@@ -13,7 +13,6 @@ export default function RiderNav({ rider }){
       <NavLink onClick={() => router.push(`/riders/${rider.user_id}`)} is_active_path={`/riders/${rider.user_id}`} className="italic">{rider.first_name} {rider.last_name}</NavLink>
       <NavSeparator/>
       <NavLink onClick={() => router.push(`/riders/${rider.user_id}/trips`)} is_active_path={`/riders/${rider.user_id}/trips`} >Trips</NavLink>
-      <NavLink onClick={() => router.push(`/riders/${rider.user_id}/stats`)} is_active_path={`/riders/${rider.user_id}/stats`}>Stats</NavLink>
     </RootNav>
   )
 }
