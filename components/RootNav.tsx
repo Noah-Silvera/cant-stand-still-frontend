4dcc4dca76361cf9ca4e3f1a504d54c73f98073d
Noah Silvera <noah@super.gd>
2021-03-25 17:48:33 -0700

    Fix broken RootNav link
    This link was broken if it was clicked from a rider's profile page.
    We should use the route to navigate to this link instead.

--- components/RootNav.tsx
+++ components/RootNav.tsx
@@ -1,10 +1,13 @@
 import Nav from './Nav'
 import NavLink from './NavLink'
+import { useRouter } from 'next/router'
 
 export default function RootNav({ children }) {
+  const router = useRouter()
+
   return (
     <Nav>
-      <NavLink href="riders" is_active_path="/riders">Riders</NavLink>
+      <NavLink onClick={() => router.push(`/riders`)} is_active_path="/riders">Riders</NavLink>
       {children}
     </Nav>
   )
