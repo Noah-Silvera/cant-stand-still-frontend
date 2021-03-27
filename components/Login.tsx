23d9ee93950d80243aa8be570b95ebe9f068756e
Noah Silvera <noah@super.gd>
2021-03-26 22:35:56 -0700

    Refactor login button to just redirect to admin side
    The login component will no longer handle authenticating with strava, so
    we can remove most of it's logic and replace it with a redirect to the
    admin side of the app.

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,87 +1,18 @@
-import { useEffect } from "react";
 import styles from "./Login.module.css"
 
 export default function Login({ className }) {
-
-  const scope = "activity:read"
-
-  useEffect(() => {
-    if(!!window){
-      const current_params = new URLSearchParams(window.location.href)
-      if(current_params.get("code") && current_params.get("scope") && window["SERVER_HOST"]){
-        (async () => {
-          const response = await fetch(`${window["SERVER_HOST"]}/login`, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-              code: current_params.get("code"),
-              scope: current_params.get("scope")
-            })
-          });
-
-          try {
-            var res = await response.json();
-          } catch(err){
-            return on_login_failure(err)
-          }
-
-          if(response.status == 200){
-            on_login_success(res["id"])
-          } else {
-            on_login_failure(res)
-          }
-        })();
-      }
-    }
-  });
-
-  const on_login_failure = (err) => {
-    if(err?.message){
-      alert(err.message)
-    } else {
-      // TODO - change this to a proper error alert
-      alert("An unknown error occured")
-    }
-  }
-
-  const on_login_success = (user_id) => {
-    sessionStorage['user_id'] = user_id
-    admin_redirect()
-  }
-
   const admin_redirect= () => {
-    window.location.replace(`https://cant-stand-still-admin.herokuapp.com/${sessionStorage["user_id"]}`)
-  }
-
-  const login_redirect = () => {
-    if(is_logged_in()){
-      admin_redirect()
+    if (window.location.hostname == "localhost") {
+      window.location.replace("http://localhost:3005/")
     } else {
-      var authParams = new URLSearchParams({
-        client_id: "22020",
-        redirect_uri: `${window["HOST"]}`,
-        response_type: "code",
-        scope: "activity:read"
-      });
-      const url = "https://www.strava.com/oauth/authorize?" + authParams.toString()
-      window.location.replace(url)
+      window.location.replace(`https://cant-stand-still-admin.herokuapp.com/`)
     }
-  }
-
-  const is_logged_in = (): boolean => {
-    if (typeof window !== 'undefined') {
-      return !!sessionStorage['user_id'];
-    }
-    return false
   }
 
   return (
     <div className={`${className} ${styles.container}`}>
       <a
-        onClick={login_redirect}
+        onClick={admin_redirect}
         id="login-logout-button"
         className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
       >Manage and create trips</a>
