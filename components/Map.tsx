e11adb4b3a91779b6b1663db4f98b6d2521e7cf3
Noah Silvera <noah@super.gd>
2021-03-27 21:16:41 -0700

    Fit map within bounds of rides

--- components/Map.tsx
+++ components/Map.tsx
@@ -7,16 +7,22 @@ import "leaflet-defaulticon-compatibility";
 import styles from '../styles/Map.module.css'
 
 const Map = ({ rides }) => {
+
+  const ridePolylines = rides.map((ride) => {
+    return (L.Polyline as any).fromEncoded(ride["map"]["summary_polyline"]);
+  })
+
+  const group = L.featureGroup(ridePolylines);
+
   return (
     <div id="map" className={styles.map}>
-      <MapContainer center={[48.493250,-123.437067]} zoom={9} className="h-full w-full">
+      <MapContainer bounds={group.getBounds().pad(0.25)} className="h-full w-full">
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {rides.map((ride) => {
-          let polyline = (L.Polyline as any).fromEncoded(ride["map"]["summary_polyline"]);
-          return <Polyline positions={polyline.getLatLngs()} key={ride["id"]}/>
+        {ridePolylines.map((polyline, idx) => {
+          return <Polyline positions={polyline.getLatLngs()} key={idx}/>
         })}
       </MapContainer>
     </div>
