5588a0f6011b208272ae2dd705770e05eb0b5435
Noah Silvera <noah@super.gd>
2021-03-27 23:21:21 -0700

    Create buffered ride polygons
    In order to improve the on hover behavior for a ride polyline, we need
    to buffer it with a polygon. We can then assign component state when
    the invisible buffered polygon is hovered over, and use this
    component state to highlight the polyline and render the ride
    information below the map.

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,12 @@
-import { MapContainer, TileLayer, Polyline, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Polyline, Polygon } from 'react-leaflet'
 import * as L from "leaflet"
 import 'polyline-encoded'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import "leaflet-defaulticon-compatibility";
 import styles from '../styles/Map.module.css'
-import { useState } from "react"
+import { useState, useRef } from "react"
+import * as turf from "@turf/turf"
 
 const Map = ({ rides }) => {
 
@@ -15,7 +16,59 @@ const Map = ({ rides }) => {
     return (L.Polyline as any).fromEncoded(ride["map"]["summary_polyline"]);
   })
 
+  const ridePolygons = ridePolylines.map((polyline: L.Polyline) => {
+    let longLatArray: number[][] = polyline.getLatLngs().map((latLng) => [latLng.lng, latLng.lat])
+    let turfLine = turf.lineString(longLatArray);
+
+
+    let buffered = turf.buffer(turfLine, 2, {units: 'kilometers'});
+    return L.GeoJSON.coordsToLatLngs(buffered.geometry.coordinates[0])
+  })
+
   const group = L.featureGroup(ridePolylines);
+
+  let lineStyle = {
+    color: "#60A5FA",
+    weight: 3
+  }
+
+  let highlightedLineStyle = {
+    color: "#2563EB",
+    weight: 4
+  }
+
+  var polylineComponents = ridePolylines.map((polyline, idx) => {
+    return (
+      <Polyline
+        key={`'ride-${idx}-polyline`}
+        positions={polyline.getLatLngs()}
+        pathOptions={currentRide?.id == rides[idx].id ? highlightedLineStyle : lineStyle}
+        ref={useRef()}
+      ></Polyline>
+    )
+  })
+
+  var polygonComponents = ridePolygons.map((polygon, idx) => {
+    return (
+      <Polygon
+        key={`'ride-${idx}-polygon-buffer`}
+        positions={polygon}
+        pathOptions={{
+          opacity: 0,
+          fillOpacity: 0,
+          color: "green"
+        }}
+        eventHandlers={{
+          mouseover: (e) => {
+            setCurrentRide(rides[idx])
+          },
+          mouseout: (e) => {
+            setCurrentRide(null)
+          }
+        }}
+      ></Polygon>
+    )
+  })
 
   return (
     <div className="flex flex-col">
@@ -25,42 +78,16 @@ const Map = ({ rides }) => {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {ridePolylines.map((polyline, idx) => {
-            let lineStyle = {
-              color: "#60A5FA",
-              weight: 3
-            }
-
-            let highlightedLineStyle = {
-              color: "#2563EB",
-              weight: 4
-            }
-            return (
-              <Polyline
-                key={idx}
-                positions={polyline.getLatLngs()}
-                pathOptions={lineStyle}
-                eventHandlers={{
-                  mouseover: (e) => {
-                    e.target.setStyle(highlightedLineStyle)
-                    setCurrentRide(rides[idx])
-                  },
-                  mouseout: (e) => {
-                    e.target.setStyle(lineStyle)
-                    setCurrentRide(null)
-                  }
-                }}
-              ></Polyline>
-            )
-          })}
+          {polygonComponents}
+          {polylineComponents}
         </MapContainer>
       </div>
       <div className="w-full">
         <div className="max-w-6xl m-auto flex flex-row justify-around p-4 text-xl">
           <p>{currentRide?.name}</p>
-          <p>Distance: {currentRide?.distance_km} km</p>
-          <p>Date: {currentRide && new Date(currentRide?.start_date).toDateString()}</p>
-          <p>Average Speed: {currentRide?.average_speed} km/h</p>
+          <p>Distance: {currentRide?.distance_km && (currentRide?.distance_km?.toFixed(1) + " km") || "..."} </p>
+          <p>Date: {currentRide && new Date(currentRide?.start_date).toDateString() || "..."}</p>
+          <p>Average Speed: {currentRide?.average_speed && (currentRide?.average_speed?.toFixed(1) + " km/h") || "..."}</p>
         </div>
       </div>
     </div>
